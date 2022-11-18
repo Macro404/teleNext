134dfc7ef4eb55abf92011d74e440a4c3c88d92b
mvedesti <larsmtv@gmail.com>
2022-11-18 16:54:47 +0100

    fixed firectory structure again

---
+++ src/main/java/telenext/TelenextApplication.java
@@ -0,0 +1,13 @@
+package telenext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelenextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelenextApplication.class, args);
+	}
+
+}
---
+++ src/test/java/telenext/TelenextApplicationTests.java
@@ -0,0 +1,13 @@
+package telenext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelenextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
