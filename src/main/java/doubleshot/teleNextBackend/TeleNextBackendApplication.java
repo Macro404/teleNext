75b4c14d32de4f79ce159056a9708f0b5f550ea8
Macro404 <marcus.westlund1994@gmail.com>
2022-11-21 09:38:39 +0100

    initialized spring boot

---
+++ src/main/java/doubleshot/teleNextBackend/TeleNextBackendApplication.java
@@ -0,0 +1,13 @@
+package doubleshot.teleNextBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeleNextBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeleNextBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/doubleshot/teleNextBackend/TeleNextBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package doubleshot.teleNextBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeleNextBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
