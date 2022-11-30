46141f3ae33c10c33ea8d530ee50d630a1d61bae
Macro404 <marcus.westlund1994@gmail.com>
2022-11-30 15:16:19 +0100

    added cors

--- src/main/java/doubleshot/teleNextBackend/controller/Controller.java
+++ src/main/java/doubleshot/teleNextBackend/controller/Controller.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://telenext.azurewebsites.net")
 public class Controller {
 
     @Autowired
